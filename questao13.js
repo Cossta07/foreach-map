1d9d1eea78058b500924d5909a3e670f60b5311a
Cossta07 <samuellinker46@gmail.com>
2024-04-22 14:45:21 -0400

    questao 13 feita

---
+++ questao13.js
@@ -0,0 +1,5 @@
+/*
+uestão 13 - Quando você usaria map() em vez de forEach()?
+Resposta:
+map() é usado para criar um novo array transformando cada elemento do array original, enquanto forEach() é usado para executar uma função em cada elemento do array sem criar um novo array.
+*/
