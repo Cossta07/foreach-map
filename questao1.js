206491ece2cd4b182e395f711e536de6ecf92270
Cossta07 <samuellinker46@gmail.com>
2024-04-22 14:09:43 -0400

    questao1 feita

---
+++ questao1.js
@@ -0,0 +1,11 @@
+/*
+Questão 01 - Crie um array com 5 números e usando o método forEach(), crie 
+um algoritmo para  adicionar 1 a cada elemento deste array?
+*/
+let array = [1, 2, 3, 4, 5];
+
+array.forEach((marans, thaua, samu) => {
+  samu[thaua] = marans + 1;
+});
+
+console.log(array);
