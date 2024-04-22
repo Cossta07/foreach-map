beab13de44c3688e420e6b9691defee3f46d81e4
Cossta07 <samuellinker46@gmail.com>
2024-04-22 14:16:42 -0400

    questao 2 e 3 feita

---
+++ questao2.js
@@ -0,0 +1,9 @@
+/*
+Questão 02 - Crie um algoritmo usando o método map() para converter um 
+array de strings para um array de números?
+*/
+let Strings = ["1", "2", "3", "4", "5"];
+
+let Numeros = Strings.map(Number);
+
+console.log(Numeros);
---
+++ questao3.js
@@ -0,0 +1,16 @@
+/*
+
+Questão 03 – Crie um algoritmo usando o método forEach() para encontrar o 
+maior número em um array de números?
+*/
+
+let array = [3,30, 2, 5];
+let maironumerodecapa = 0;
+
+array.forEach(ffmilgrau => {
+  if (ffmilgrau > maironumerodecapa) {
+    maironumerodecapa = ffmilgrau;
+  }
+});
+
+console.log(maironumerodecapa);
