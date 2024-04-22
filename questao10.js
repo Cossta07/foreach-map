07ece6b7792db2468b253fc8dbde81e790d3d8b8
Cossta07 <samuellinker46@gmail.com>
2024-04-22 14:39:12 -0400

    questao 9 e 10 feitas

---
+++ questao10.js
@@ -0,0 +1,6 @@
+/*
+É possível modificar o array original com map()?
+Resposta:
+Não, o método map() não modifica o array original. Ele retorna um novo array com base nas operações realizadas em cada elemento do array original, sem alterar o array original.
+
+*/
---
+++ questao9.js
@@ -0,0 +1,6 @@
+/*
+Qual é a diferença fundamental entre map() e forEach()?
+Resposta:
+Map() e forEach() são duas maneiras de alterar um array. Map() cria um novo array com as alterações, enquanto forEach() apenas percorre o array original e faz algo com cada elemento, mas não altera o array original.
+
+*/
