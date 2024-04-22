fc8fe7319d8225046e66b80447c4eadf5ea1e378
Cossta07 <samuellinker46@gmail.com>
2024-04-22 14:48:42 -0400

    questao 14 feita

---
+++ questao14.js
@@ -0,0 +1,5 @@
+/*
+Questão 14 - É possível encadear map() e forEach() juntos? 
+Resposta:
+Sim, você pode vincular map() e forEach() em JavaScript. map() cria um novo array alterando cada elemento do array antigo, enquanto forEach() percorre os elementos sem criar um novo array.
+*/
