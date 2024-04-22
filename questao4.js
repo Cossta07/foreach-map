bb5af170cef986167a2cc79a5b2233a486f8cca2
Cossta07 <samuellinker46@gmail.com>
2024-04-22 14:43:54 -0400

    questao 11,12 e 4 feitas

---
+++ questao4.js
@@ -0,0 +1,10 @@
+/*
+Questão 04 – Crie um algoritmo usando o método forEach() para encontrar a 
+soma de todos os números em um array?
+*/
+let array = [1, 10000, 3, 1114, 101];
+let eisso = 0;
+array.forEach(vamopo => {
+  eisso += vamopo;
+});
+console.log(eisso);
